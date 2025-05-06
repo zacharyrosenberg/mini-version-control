bace733abe8dae0a6da3e9bd493a45d8ccb2a43c
Zachary Rosenberg <zacharyrosenberg41@gmail.com>
2025-05-05 20:43:02 -0400

    create index class

---
+++ src/main/java/minivcs/Index.java
@@ -0,0 +1,14 @@
+package minivcs;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class Index {
+
+    private static final String INDEX_FILE = ".minivcs/index";
+
+    public static void add(Path repoPath, Path filepath) throws IOException {
+
+    }
+
+}
